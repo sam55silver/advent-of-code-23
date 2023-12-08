4a019d6eb5dfdd07f450c276ac5b3281be812610
Sam Silver <ssilver@kinesin.ca>
2023-12-08 16:04:38 -0400

    first attempt part2 - wrong

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,58 @@
 use std::{env, io::{self, BufRead, BufReader}, fs::File};
 
+
 fn get_calibration(line: &str) -> u32 {
     let mut nums: Vec<u32> = Vec::new();
+    let mut word = String::new();
 
     for ch in line.chars() {
+        // If a num then add to list of nums
+        // If not, add it to
         match ch.to_digit(10) {
-            Some(num) => nums.push(num),
-            None => continue
+            Some(num) => {
+                nums.push(num);
+                word = String::new();
+            },
+            None => word.push(ch)
         }
+        
+        if word.contains("one") {
+            nums.push(1);
+            word = String::new();
+        } 
+        else if word.contains("two") { 
+            nums.push(2);
+            word = String::new();
+        } 
+        else if word.contains("three") { 
+            nums.push(3);
+            word = String::new();
+        } 
+        else if word.contains("four") {
+            nums.push(4);
+            word = String::new();
+        } 
+        else if word.contains("five") { 
+            nums.push(5);
+            word = String::new();
+        } 
+        else if word.contains("six") { 
+            nums.push(6);
+            word = String::new();
+        } 
+        else if word.contains("seven") { 
+            nums.push(7);
+            word = String::new();
+        } 
+        else if word.contains("eight") {
+            nums.push(8);
+            word = String::new();
+
+        } 
+        else if word.contains("nine") { 
+            nums.push(9);
+            word = String::new();
+        }; 
     }
 
     return (nums[0] * 10) + nums.last().unwrap();
@@ -41,6 +86,10 @@ fn main() {
             std::process::exit(1);
         }
     };
+
+    for num in &nums {
+        println!("{num}");
+    }
 
     let sum: u32 = nums.iter().sum();
     println!("Calibration Values: {sum}");
