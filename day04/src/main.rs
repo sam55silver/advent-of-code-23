e04067c06bcbc7a6c2a64a51a8f874635858d160
Sam Silver <ssilver@kinesin.ca>
2023-12-12 10:52:42 -0400

    Made a better day04

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,87 +1,89 @@
 use std::{
     env,
     fs::File,
-    io::{BufRead, BufReader}, time::Instant,
+    io::{BufRead, BufReader}, time::Instant, u32,
 };
 
 #[derive(Clone)]
 #[derive(Debug)]
 struct Card {
-    id: u32,
-    winning_nums: Vec<u32>,
-    listed_nums: Vec<u32>,
-}
-
-impl Card {
-    fn num_wins(&self) -> Vec<bool> {
-        self.winning_nums
-            .iter()
-            .map(|&num| self.listed_nums.contains(&num))
-            .collect()
-    }
-
-    fn score(&self) -> u32 {
-        let mut score = 0;
-        self.num_wins()
-            .iter()
-            .for_each(|win| if *win {
-                if score == 0 { score = 1}
-                else { score *= 2}
-            }); 
-        
-        score
-    }
-
+    wins: u32,
+    score: u32,
 }
 
 fn create_card(line: &str) -> Card {
-    let mut split = line.split(':');
-    let id: u32 = split.next().unwrap().get(5..).unwrap().trim().parse().unwrap();
-    let mut numbers = split.next().unwrap().split('|');
-    let winning_nums = numbers
+    let mut numbers = line.split(':').last().unwrap().split('|');
+
+    let winning_nums: Vec<u32> = numbers
         .next()
         .unwrap()
         .split_whitespace()
         .map(|num| num.parse::<u32>().unwrap())
         .collect();
 
-    let listed_nums = numbers
+    let listed_nums: Vec<u32> = numbers
         .next()
         .unwrap()
         .split_whitespace()
         .map(|num| num.parse::<u32>().unwrap())
         .collect();
+
+    let wins: u32 = winning_nums
+        .iter()
+        .map(|&num| listed_nums.contains(&num))
+        .filter(|b| *b)
+        .count()
+        .try_into()
+        .unwrap();
+    
+    let score: u32 = wins.pow(2);
 
     Card {
-        id,
-        winning_nums,
-        listed_nums,
+        wins,
+        score,
     }
 }
 
-fn read_cards(path: &str) -> Vec<Card> {
+fn read_cards(path: &str) -> (Vec<Card>, u32) {
     let file = File::open(path).expect("Failed to open path: {path}");
     let reader = BufReader::new(file);
 
+    let mut max_wins: u32 = 0;
     let cards: Vec<Card> = reader
         .lines()
-        .map(|line| create_card(&line.unwrap()))
+        .map(|line| {
+            let card = create_card(&line.unwrap());
+            if card.wins > max_wins { max_wins = card.wins }
+            card
+        })
         .collect();
 
-    cards
+    (cards, max_wins)
 }
 
-fn find_copies(cards: &Vec<Card>, id: u32) -> Vec<u32> {
-    let card = &cards[(id - 1) as usize];
-    let mut copies = vec![id];
-    let mut match_id = id;
-    for num in &card.winning_nums {
-        if card.listed_nums.contains(num) {
-            match_id += 1;
-            copies.extend(find_copies(cards, match_id))
+fn circular_buff(tickets: &Vec<Card>, max_wins: u32) -> u32 {
+    let mut total_tickets: u32 = 0;
+
+    // Highest num of wins will only ever be max_wins
+    // Therefor make buffer max_wins + 1 so you can store current card's number of wins and the cards after that
+    // Even if a ticket does not win, it itself counts as one, therefor make buff all 1's
+    let mut buff: Vec<u32> = vec![1; (max_wins + 1) as usize];
+
+    for card in tickets {
+        for i in 1..=card.wins {
+            // Add the current amount of wins to the next
+            // sequential numbers
+            buff[i as usize] += buff[0];
         }
+
+        // Pop off current card and add to total ticket count
+        total_tickets += buff[0];
+
+        buff.push(1);
+        buff.remove(0);
     }
-    return copies;
+
+    total_tickets
 }
 
 fn main() {
@@ -94,20 +96,18 @@ fn main() {
 
     let path_inputs = &args[1];
     let part = &args[2];
-    let cards = read_cards(path_inputs);
 
     let start_time = Instant::now();
 
+    let (cards, max_wins) = read_cards(path_inputs);
+
     if part == "1" {
-        let total_score: u32 = cards.iter().map(|card| card.score()).sum();
+        let total_score: u32 = cards.iter().map(|card| card.score).sum();
         println!("Total score: {}", total_score);
     }
     else if part == "2" {
-        let mut total_copies: Vec<u32> = Vec::new();
-        for card in &cards {
-            total_copies.extend(find_copies(&cards, card.id));
-        }
-        println!("{:?}", total_copies.len());
+        let total_tickets = circular_buff(&cards, max_wins);
+        println!("Total tickets: {}", total_tickets); 
     }
 
     let end_time = Instant::now();
--- day04/stats.md
+++ day04/stats.md
@@ -1,8 +1,10 @@
 # Day 04
 
-Lots of recursion was used to solve day 04. This caused my solution to be really slow with no optimizations.
+I initially used a lot of recursion to solve day 04. This caused my solution to be really slow with no optimizations.
 Using rust's --release flag on build reduced the time by quite a bit, here are the original results
 
 `Original speed - Debug: 24315 milliseconds, Release: 1617 milliseconds.`
 
-This is still very slow so I decided to try and speed things up with caching.
+I decided to take a different approach. I realized there was a max amount of wins that could happen for a single ticket. This means that there is a set amount of cards after the current card that is being analyzed. So, instead of looping multiple times into the wins of a card like a tree I could use a circular buffer. I would fill the buffer with the max amount of wins + 1 (for the current card being looked at for wins) and fill each space with a 1 (for the original ticket). I would then take the current card, get it's wins, and also get how many cards of that id I currently have. I would then add the amount of cards I currently have to next following cards, the number of wins I have determines how many of the next amount of cards I add it too. 
+
+With a few tweaks to the card reader as well but finding the amount of wins during the first read of the card, I was able to get the total amount of cards within `3 milliseconds`!
