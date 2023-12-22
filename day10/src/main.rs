d13f03ca502771917e1bc33fa7a11a0a3f8520cb
Sam Silver <sam55silver@gmail.com>
2023-12-22 17:54:40 -0400

    init day10

---
+++ day10/src/main.rs
@@ -0,0 +1,147 @@
+use std::fmt::{Display, Result, Formatter};
+
+use utilities::get_input_lines;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Pipes {
+    NS,
+    EW,
+    NE,
+    NW,
+    SW,
+    SE,
+    Ground,
+    Start
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+struct Map {
+    map: Vec<Vec<Pipes>>,
+    start: Pos
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        for row in &self.map {
+            for item in row {
+                write!(f, "{:?} ", item)?; // Display each enum item
+            }
+            write!(f, "\n")?; // Add a new line after each row
+        }
+        Ok(())
+    }
+}
+
+impl Map {
+    fn new(raw_map: Vec<Vec<char>>) -> Map {
+        let mut start = Pos { x: 0, y: 0 };
+        let map: Vec<Vec<Pipes>> = raw_map.iter().enumerate().map(|(row_i, row)| {
+            row.iter().enumerate().map(|(c_i, c)| {
+                match c {
+                    '|' => Pipes::NS,
+                    '-' => Pipes::EW,
+                    'L' => Pipes::NE,
+                    'J' => Pipes::NW,
+                    '7' => Pipes::SW,
+                    'F' => Pipes::SE,
+                    '.' => Pipes::Ground,
+                    'S' => {
+                        start = Pos { x: c_i, y: row_i };
+                        Pipes::Start
+                    },
+                    _ => panic!("unknown pipe!")
+                }
+            }).collect()
+        }).collect();
+
+        Map { map, start }
+    }
+
+    fn get_pipe(&self, pos: Pos) -> Option<&Pipes> {
+        match self.map.get(pos.y) {
+            Some(row) => row.get(pos.x),
+            None => None
+        }
+    }
+
+    // TODO: Make a get_pos fn to return some(pos) only if in map boundary
+    fn find_connections(&self, pos: Pos) -> Vec<Pos> {
+        println!("Pos: {:?}", pos);
+        let up = Pos { x: pos.x, y: pos.y - 1 };
+        let down = Pos { x: pos.x, y: pos.y + 1 };
+        let left = Pos { x: pos.x - 1, y: pos.y };
+        let right = Pos { x: pos.x + 1, y: pos.y };
+
+        let mut locs: Vec<Pos> = Vec::new();
+        match self.get_pipe(up) {
+            Some(pipe) => {
+                match pipe {
+                    Pipes::SW => locs.push(up),
+                    Pipes::SE => locs.push(up),
+                    Pipes::NS => locs.push(up),
+                    _ => ()
+                }
+            },
+            None => ()
+        }
+        match self.get_pipe(down) {
+            Some(pipe) => match pipe {
+                Pipes::NE => locs.push(down),
+                Pipes::NW => locs.push(down),
+                Pipes::NS => locs.push(down),
+                _ => ()
+            },
+            None => ()
+        }
+        match self.get_pipe(left) {
+            Some(pipe) => match pipe {
+                Pipes::SE => locs.push(left),
+                Pipes::NE => locs.push(left),
+                Pipes::EW => locs.push(left),
+                _ => ()
+            },
+            None => ()
+        }
+        match self.get_pipe(right) {
+            Some(pipe) => match pipe {
+                Pipes::NW => locs.push(right),
+                Pipes::SW => locs.push(right),
+                Pipes::EW => locs.push(right),
+                _ => ()
+            },
+            None => ()
+        }
+
+        locs
+    }
+}
+
+fn main() {
+    let (lines, _part) = get_input_lines();
+
+    let map = Map::new(lines.iter().map(|line| line.chars().collect()).collect());
+    println!("{}", map);
+    let postions_start = map.find_connections(map.start);
+    let mut last_pos = map.start;
+    let mut curr_pos = postions_start[0];
+    loop {
+        let pipe = map.get_pipe(curr_pos);
+        println!("Pipe: {:?}", pipe.unwrap());
+
+        let next_positions: Vec<Pos> = map.find_connections(curr_pos);
+        match next_positions.iter().filter(|p| **p != last_pos).collect::<Vec<&Pos>>().first() {
+            Some(forward_pos) => {
+                last_pos = curr_pos;
+                curr_pos = **forward_pos;
+            },
+            None => break
+        }
+       
+    }
+    println!("Loop complete")
+}
